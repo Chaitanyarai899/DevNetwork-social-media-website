033c371c8e80f870b4aa346f59af64aeb0a0f057
Chaitanya Rai <Chaitanyarai899@gmail.com>
2023-06-11 14:36:36 +0530

    server link changed

--- client/src/axios.js
+++ client/src/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const makeRequest = axios.create({
-  baseURL: "http://localhost:8800/api/",
+  baseURL: "https://devnetworkserver.onrender.com/api/",
   withCredentials: true,
 });
